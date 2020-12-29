7fad567a445cd4b8b881b95af44de9a4a416daa0
Olivia Guzman <olguva@gmail.com>
2020-12-28 22:42:06 -0600

    Completed Angular Lesson 1 Exercise part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://cdn.pixabay.com/photo/2016/04/18/22/05/sea-1337565__340.jpg';
+  image3 = 'https://media.istockphoto.com/photos/shrimp-street-tacos-picture-id1200136076?b=1&k=6&m=1200136076&s=170667a&w=0&h=fhLFL-P66KifMk7iy51zn9qLt9XxhEHfbHyw2FtbVqs=';
 
   constructor() { }
 
